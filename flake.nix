0be5839a98b0953b673016563c93e12a36bfe772
thegergo02 <work@varigergo.xyz>
2022-06-28 11:57:13 +0200

    fix: build source paths

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = github:nixos/nixpkgs/nixos-21.11;
     flake-utils.url = github:numtide/flake-utils;
     gomod2nix.url = github:tweag/gomod2nix;
+    #gomod2nix.url = github:thegergo02/gomod2nix;
     grpc-gateway.url = github:thegergo02/grpc-gateway-flake;
     protoc-gen-validate.url = github:thegergo02/protoc-gen-validate-flake;
     protoc-gen-authoption.url = github:thegergo02/protoc-gen-authoption-flake;
@@ -35,11 +36,6 @@
         };
         
         setup = pkgs.writeScriptBin "setup" ''
-          set -xeu
-
-          export GOPATH=$(pwd)/gopath
-          export SRC_PATH=$GOPATH/src/github.com/zitadel/zitadel
-
           export GATEWAY_VERSION=2.6.0
           export VALIDATOR_VERSION=0.6.2
 
@@ -47,18 +43,15 @@
           export PROTO_INC_PATH=$PROTO_PATH/include
           export PROTO_ZITADEL_PATH=$PROTO_INC_PATH/zitadel
 
-          export ZITADEL_PATH=$GOPATH/src/github.com/zitadel/zitadel
+          export ZITADEL_PATH=/build/source
           export DOCS_PATH=$ZITADEL_PATH/docs/apis/proto
           export OPENAPI_PATH=$ZITADEL_PATH/openapi/v2
           export GRPC_PATH=$ZITADEL_PATH/pkg/grpc
-          
-          # copy source to gopath
-          mkdir -p $SRC_PATH
-          pushd $SRC_PATH
 
-          cp -r ${zitadel-src}/* .
-          chmod -R +w $ZITADEL_PATH
+          mkdir -p $GOPATH/src/github.com/zitadel
+          ln -sf $ZITADEL_PATH $GOPATH/src/github.com/zitadel/zitadel
 
+          pushd $ZITADEL_PATH
           # proto files
           mkdir -p $PROTO_INC_PATH/validate
           cp ${protoc-gen-validate.defaultPackage.${system}}/validate.proto $PROTO_INC_PATH/validate
@@ -70,7 +63,6 @@
           cp ${googleapis}/google/api/http.proto $PROTO_INC_PATH/google/api/.
           cp ${googleapis}/google/api/field_behavior.proto $PROTO_INC_PATH/google/api/.
           cp -r $ZITADEL_PATH/proto/* $PROTO_INC_PATH/.
-
           popd
         '';
         gen-statik0 = (pkgs.writeScriptBin "gen-statik0" (builtins.readFile ./scripts/generate-statik0.sh)).overrideAttrs(old: {
@@ -89,7 +81,7 @@
           ${gen-statik0}/bin/gen-statik0
           ${gen-grpc}/bin/gen-grpc
           ${gen-statik1}/bin/gen-statik1
-          ${gen-assets}/bin/gen-assets
+          #${gen-assets}/bin/gen-assets
         '';
       in
       rec {
@@ -98,6 +90,7 @@
               name = "zitadel-base";
               src = "${zitadel-src}";
               modules = ./gomod2nix.toml;
+              go = pkgs.go_1_17;
               subPackages = [ "cmd/zitadel" ];
               nativeBuildInputs = with pkgs; [
                 protobuf3_18
@@ -116,7 +109,7 @@
                 gen-statik0 gen-grpc gen-statik1 gen-assets gen 
               ];
               buildInput = [ pkgs.cockroachdb ];
-              postConfigure = ''
+              preBuild = ''
                 source ${setup}/bin/setup
                 ${gen}/bin/gen
               '';
